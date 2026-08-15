39f726ee9d694846db5e31c5b9e5d8b567035c49
Tim Crowe <tim.crowe.dev@proton.me>
2026-08-14 21:38:03 -0500

    restructure factor module

---
+++ src/factor.rs
@@ -0,0 +1,31 @@
+pub mod factor_kind;
+pub mod dense_factor;
+pub mod unary_factor;
+
+mod log_utils;
+mod utils;
+
+
+pub use factor_kind::FactorKind;
+pub use dense_factor::DenseFactor;
+pub use unary_factor::UnaryFactor;
+
+/// A factor in the sense used in probabilistic graphical models.
+pub trait Factor {
+    /// Variables in the factor's scope.
+    fn scope(&self) -> &[usize];
+
+    /// The number of variables in the factor.
+    fn ndim(&self) -> usize {
+        return self.scope().len();
+    }
+
+    /// Consume self and marginalize the given variables.
+    fn marginalize(self, vars: &[usize]) -> FactorKind;
+}
+
+/// A factor over discrete variables with finite cardinalities.
+pub trait DiscreteFactor: Factor {
+    /// Cardinalities of all variables in scope.
+    fn card(&self) -> &[usize];
+}
--- src/factor/discrete_factor.rs
+++
@@ -1,9 +0,0 @@
-//! Trait for factors over discrete variables with finite cardinalities.
-
-use super::Factor;
-
-/// A factor over discrete variables with finite cardinalities.
-pub trait DiscreteFactor: Factor {
-    /// Cardinalities of all variables in scope.
-    fn card(&self) -> &[usize];
-}
