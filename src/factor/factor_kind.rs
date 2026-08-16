a6ad35372797a6609cdc364a515ac76739670c97
Tim Crowe <tim.crowe.dev@proton.me>
2026-08-16 11:27:58 -0500

    simplify FactorKind::marginalize

--- src/factor/factor_kind.rs
+++ src/factor/factor_kind.rs
@@ -1,6 +1,4 @@
 use super::{Factor, DenseFactor, ScalarFactor, UnaryFactor};
-use super::log_utils::lse_two_pass;
-use super::utils::dense_into_unary;
 
 #[derive(Clone, Debug)]
 pub enum FactorKind {
@@ -21,25 +19,9 @@ impl FactorKind {
     // Consuming marginalize: takes ownership and returns a new FactorKind
     pub fn marginalize(self, vars: &[usize]) -> FactorKind {
         match self {
-            FactorKind::Dense(d) => {
-                let reduced = d.marginalize_kernel(vars); // returns DenseFactor
-                match reduced.scope().len() {
-                    0 => FactorKind::Scalar(ScalarFactor::new(reduced.data()[[]])),
-                    1 => FactorKind::Unary(dense_into_unary(reduced)),
-                    _ => FactorKind::Dense(reduced),
-                }
-            }
-            FactorKind::Scalar(s) => {
-                FactorKind::Scalar(s)
-            }
-            FactorKind::Unary(u) => {
-                if vars.contains(&u.scope()[0]) {
-                    let total = lse_two_pass(&u.data());
-                    FactorKind::Unary(UnaryFactor::new(u.scope()[0], vec![total]))
-                } else {
-                    FactorKind::Unary(u)
-                }
-            }
+            FactorKind::Dense(d) => d.marginalize(vars),
+            FactorKind::Unary(u) => u.marginalize(vars),
+            FactorKind::Scalar(s) => s.marginalize(vars),
         }
     }
 }
